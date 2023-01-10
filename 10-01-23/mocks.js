1920a6d8fe82bdac04707b6749ba489ed070947b
alekyari <117306375+alekyari@users.noreply.github.com>
2023-01-10 18:02:31 +0100

    Add files via upload

---
+++ 10-01-23/mocks.js
@@ -0,0 +1,40 @@
+const products = [
+  {
+    id: 1,
+    title: "primo prodotto",
+    description: "descrizione del primo prodotto",
+    img: "https://picsum.photos/200/200?1",
+  },
+  {
+    id: 2,
+    title: "secondo prodotto",
+    description: "descrizione del secondo prodotto",
+    img: "https://picsum.photos/200/200?2",
+  },
+  {
+    id: 3,
+    title: "terzo prodotto",
+    description: "descrizione del terzo prodotto",
+    img: "https://picsum.photos/200/200?3",
+  },
+  {
+    id: 4,
+    title: "quarto prodotto",
+    description: "descrizione del primo prodotto",
+    img: "https://picsum.photos/200/200?4",
+  },
+  {
+    id: 5,
+    title: "quinto prodotto",
+    description: "descrizione del secondo prodotto",
+    img: "https://picsum.photos/200/200?5",
+  },
+  {
+    id: 6,
+    title: "sesto prodotto",
+    description: "descrizione del terzo prodotto",
+    img: "https://picsum.photos/200/200?6",
+  },
+];
+
+export { products };
